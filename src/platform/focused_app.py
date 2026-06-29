925796738a8cf8488e2f1d1c9eaaa87b373aeb32
Duy Nguyen <duy28092005yh@gmail.com>
2026-06-29 09:30:11 +0700

    style: format focused platform shell

--- src/platform/focused_app.py
+++ src/platform/focused_app.py
@@ -3,13 +3,14 @@
 from src.platform.bootstrap import build_default_registry, build_platform_context
 from src.platform.config import PlatformConfig
 from src.platform.games import GameExitAction, GameLaunchOptions, GameRegistry
-from src.platform.scenes.arcade_shelf_scene import run_arcade_shelf_scene
 from src.platform.scenes.base import PlatformAction
 from src.platform.scenes.game_details_scene import run_about_scene
 from src.platform.scenes.leaderboard_scene import run_leaderboard_scene
 from src.platform.scenes.rewards_scene import run_rewards_scene
 from src.platform.scenes.settings_scene import run_settings_scene
 from src.platform.scenes.studio_intro_scene import run_studio_intro_scene
+
+from src.platform.scenes.arcade_shelf_scene import run_arcade_shelf_scene
 
 
 class FocusedPlatformApp:
@@ -33,30 +34,9 @@ class FocusedPlatformApp:
 
         try:
             while scene != PlatformAction.QUIT:
-                if scene in (PlatformAction.HOME, PlatformAction.GAME_LIBRARY):
-                    result = run_arcade_shelf_scene(pygame, context, self.registry)
-                elif scene == PlatformAction.SETTINGS:
-                    result = run_settings_scene(pygame, context)
-                elif scene == PlatformAction.ABOUT:
-                    result = run_about_scene(pygame, context)
-                elif scene == PlatformAction.LEADERBOARD:
-                    result = run_leaderboard_scene(pygame, context)
-                elif scene == PlatformAction.REWARDS:
-                    result = run_rewards_scene(pygame, context)
-                else:
-                    result = run_arcade_shelf_scene(pygame, context, self.registry)
-
+                result = self._run_scene(pygame, context, scene)
                 if result.action == PlatformAction.LAUNCH_GAME and result.game_id:
-                    game = self.registry.get(result.game_id)
-                    options = self._configure_launch(game, context)
-                    if options is None:
-                        scene = PlatformAction.GAME_LIBRARY
-                        continue
-                    exit_result = game.create_session(context, options).run()
-                    if exit_result.action == GameExitAction.QUIT:
-                        scene = PlatformAction.QUIT
-                    else:
-                        scene = PlatformAction.GAME_LIBRARY
+                    scene = self._launch_game(context, result.game_id)
                     context.audio.play_menu_music()
                 else:
                     scene = result.action
@@ -64,6 +44,29 @@ class FocusedPlatformApp:
             context.save.save(context.settings.to_document())
             pygame.quit()
         return 0
+
+    def _run_scene(self, pygame, context, scene):
+        if scene in (PlatformAction.HOME, PlatformAction.GAME_LIBRARY):
+            return run_arcade_shelf_scene(pygame, context, self.registry)
+        if scene == PlatformAction.SETTINGS:
+            return run_settings_scene(pygame, context)
+        if scene == PlatformAction.ABOUT:
+            return run_about_scene(pygame, context)
+        if scene == PlatformAction.LEADERBOARD:
+            return run_leaderboard_scene(pygame, context)
+        if scene == PlatformAction.REWARDS:
+            return run_rewards_scene(pygame, context)
+        return run_arcade_shelf_scene(pygame, context, self.registry)
+
+    def _launch_game(self, context, game_id: str) -> PlatformAction:
+        game = self.registry.get(game_id)
+        options = self._configure_launch(game, context)
+        if options is None:
+            return PlatformAction.GAME_LIBRARY
+        exit_result = game.create_session(context, options).run()
+        if exit_result.action == GameExitAction.QUIT:
+            return PlatformAction.QUIT
+        return PlatformAction.GAME_LIBRARY
 
     @staticmethod
     def _configure_launch(game, context):
